d6f2ffcebece9383f5311073003097c4e5827615
LadaPecnikova <165075127+LadaPecnikova@users.noreply.github.com>
2024-04-11 20:17:41 +0200

    Uvod

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Vitej v kurzu C# 2!");
+Console.WriteLine("Bude to jizda!");
